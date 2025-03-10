56e07bf5a758ffd7d761b90436e02a2c623807a1
Thanh Tung <entii2003@gmail.com>
2025-03-10 09:15:58 +0700

    get emotion

--- WebDoDienTu/WebDoDienTu/Controllers/ProductReviewController.cs
+++ WebDoDienTu/WebDoDienTu/Controllers/ProductReviewController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text;
 using WebDoDienTu.Data;
 using WebDoDienTu.Models;
+
 
 namespace WebDoDienTu.Controllers
 {
@@ -20,7 +23,38 @@ namespace WebDoDienTu.Controllers
         [HttpPost]
         public async Task<IActionResult> AddReview(int productId,string name, string email, int rating, string comment)
         {
-            var user = await _userManager.GetUserAsync(User);
+			var emotion = "";
+			using (HttpClient client = new HttpClient())
+			{
+				var requestData = new { Comment = comment }; // Dữ liệu gửi đi
+				var jsonRequest = JsonSerializer.Serialize(requestData);
+				var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+				var response = await client.PostAsync("http://127.0.0.1:5000/insert", content);
+
+				if (response.IsSuccessStatusCode)
+				{
+					try
+					{
+						var jsonResponse = await response.Content.ReadAsStringAsync();
+						var data = JsonSerializer.Deserialize<JsonElement>(jsonResponse);
+
+						// Lấy giá trị của "message"
+						emotion = data.GetProperty("message").GetString();
+						//emotion = data?["message"];
+						//Console.WriteLine(emotion);
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex);
+					}
+				}
+				else
+				{
+					return Json(new { success = false, message = "Lỗi khi gọi API!" });
+				}
+			}
+			var user = await _userManager.GetUserAsync(User);
             if (!User.Identity.IsAuthenticated)
             {
                 return Json(new { success = false, message = "Vui lòng đăng nhập để thực hiện đánh giá!" });
@@ -33,8 +67,10 @@ namespace WebDoDienTu.Controllers
                 YourName = name,
                 YourEmail = email,
                 Rating = rating,
-                Comment = comment
-            };
+                Comment = comment,
+				Emotional = emotion
+				
+			};
 
             _context.ProductReviews.Add(review);
             await _context.SaveChangesAsync();
@@ -42,6 +78,7 @@ namespace WebDoDienTu.Controllers
             TempData["SuccessMessage"] = "Your review has been submitted successfully!";
 
             return Json(new { success = true, message = "Your review has been submitted successfully!" });
+
         }
     }
 }
